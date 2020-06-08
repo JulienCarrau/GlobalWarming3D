91fb767da4e969cb669df2ac2a7bd7c9c4b65749
JulienCarrau <juliencarrau@orange.fr>
2020-06-08 11:27:22 +0200

    Restructurate

---
+++ src/sample/data/DataLoader.java
@@ -0,0 +1,20 @@
+/*try {
+	FileReader file = new FileReader("name_fichier.ext");
+	BufferedReader bufRead = new BufferedReader(file);
+
+	String line = bufRead.readLine();
+	while ( line != null) {
+	   	String[] array = line.split(",");
+	   
+	    int id = Integer.parseInt(array[0]);
+	    float val = Float.parseFloat(array[6]);
+	        		
+	    line = bufRead.readLine();
+	}
+
+	bufRead.close();
+	file.close();
+	
+} catch (IOException e) {
+	e.printStackTrace();
+}*/
