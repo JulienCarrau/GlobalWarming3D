1d7309a9e9b4e9cc7c0445238a1b053708763f84
JulienCarrau <juliencarrau@orange.fr>
2020-06-08 11:38:49 +0200

    Few modif

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,14 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("GlobalWarming3D");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();*/
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
     }
 }
