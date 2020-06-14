8a4df4e54b62477fcbb8803429116857f0d541e9
JulienCarrau <juliencarrau@orange.fr>
2020-06-14 12:05:15 +0200

    Change year position

--- src/sample/gui/view2D/YearView.java
+++ src/sample/gui/view2D/YearView.java
@@ -3,6 +3,7 @@ package sample.gui.view2D;
 import javafx.scene.Group;
 import javafx.scene.SubScene;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class YearView {
@@ -12,12 +13,13 @@ public class YearView {
     public YearView(Pane pane, int year) {
         root = new Group();
         this.year = new Text();
-        this.year.setTranslateY(10);
+        this.year.setFont(new Font(20));
+        this.year.setTranslateY(20);
         root.getChildren().add(this.year);
 
-        SubScene scene = new SubScene(root, 30, 10);
-        scene.setLayoutX(950);
-        scene.setLayoutY(600);
+        SubScene scene = new SubScene(root, 50, 25);
+        scene.setLayoutX(20);
+        scene.setLayoutY(20);
         pane.getChildren().add(scene);
 
         setYear(year);
